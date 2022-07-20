dd14e548ff5567891e59e998fcaa1fa99f7e0f45
Homework1 <danisholha760@gmail.com>
2022-07-20 14:54:10 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(" " + i);
+            i++;
+        }
+        System.out.println();
+        for (int n = 10; n >= 1; n--) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        // Task 2
+
+        for (int i1 = 3; i1 <= 31; i1 = i1 + 7) {
+            System.out.println("Today is Friday the " + i1 + "th." + "Need to prepare a report");
+        }
+
+        // Task 3
+
+        int cometFliesEvery = 79;
+        int currentYear = 2022;
+        int upToTheCurrentYear = currentYear - 200;
+        int afterCurrentYear = currentYear + 100;
+        for (int i2 = 0; i2 <= afterCurrentYear; i2 = i2 + cometFliesEvery) {
+            if (upToTheCurrentYear < i2) {
+                System.out.println(" " + i2);
+            }
+        }
+
+
+
+
+
+
     }
 }
